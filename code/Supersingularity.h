9744b1710f966a89ce3eee4cee0f28935bd54ec0
ECCTF <32855008+ECCTF@users.noreply.github.com>
2025-10-09 16:55:43 +0800

    Delete code directory

--- code/Supersingularity.h
+++
@@ -1,9 +0,0 @@
-#include "pointOperation.h"
-#include "scalarMultiplication.h"
-#pragma once
- 
-
-int productTree(big A, int &IsSupersingularity);
-int newProductTree(big A, doubleBaseType DBCTerm[], int DBCLength, int &IsSupersingularity);
-
-int pairingMontgomeryLadder(big A, int &IsSupersingularity);
